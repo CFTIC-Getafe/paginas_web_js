ba9eda36dc1353448360d2fc72a77ed6dae0ce4c
alce65 <alce65@hotmail.es>
2018-06-21 14:06:09 +0200

    Hola Mundo

---
+++ 01_Datos/Hola_Funcion.js
@@ -0,0 +1,17 @@
+/*
+  JavaScript (@Oracle) -> ECMAScript -> ES6 / ES2015
+ */
+
+function saludar(pUser) {
+    const SALUDO = 'Hola'
+    console.log(SALUDO, pUser)
+}
+
+function kraus(a,b) {
+    let resultado = a + b
+    console.log(resultado)
+}
+
+saludar('Pepe')
+saludar('Elena')
+kraus(2,4)
