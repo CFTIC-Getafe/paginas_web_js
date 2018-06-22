804ea0fcc9350f20b3092558e04f1f402ade5d02
alce65 <alce65@hotmail.es>
2018-06-22 11:30:23 +0200

    Datos y funciones

--- 01_Datos/Hola_Mundo.js
+++ 01_Datos/Hola_Mundo.js
@@ -2,6 +2,11 @@
 var saludo
 /** Inicializacion */
 saludo = 'Hola'
+
+/** Declaracion + Inicializacion */
+var user = 'Mundo'
+
+
 /*
 Tipos de datos
 - string
@@ -11,7 +16,29 @@ Tipos de datos
 - undefined
 */
 
-/** Declaracion + Inicializacion */
-var user = 'Mundo'
+
+
+/** Tipos de Datos */
+var nombre = 'Pepe' // string
+var edad = 23 // number
+var isEstudiante = false // boolean
+var algo // undefined
+// Objeto en notacion JSON
+var persona = {
+    nombre: 'Juan',
+    edad: 34,
+    isEstudiante: true
+}
+
+/** Como es JS respecto a los tipos
+ * - debil 
+ * - dinámico
+ */
 
 console.log(saludo, user)
+console.log(nombre, typeof nombre)
+console.log(edad, typeof edad)
+console.log(isEstudiante, typeof isEstudiante)
+console.log(algo, typeof algo)
+console.log(persona, typeof persona)
+console.log(persona.nombre, typeof persona.nombre)
