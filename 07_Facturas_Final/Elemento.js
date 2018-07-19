4c47131af3cde353591df40492528db366122c50
alce65 <alce65@hotmail.es>
2018-07-19 13:53:16 +0200

    Factura Web Final

---
+++ 07_Facturas_Final/Elemento.js
@@ -0,0 +1,10 @@
+export class Elemento {
+    constructor (descripcion, precio, cantidad) {
+        this.descripcion = descripcion
+        this.precio = precio
+        this.cantidad = cantidad
+    }
+}
+
+
+
